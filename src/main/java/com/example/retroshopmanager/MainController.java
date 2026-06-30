bf75a0756f9e767bb5eedad5aeb60454f6206822
YuliiaHula <yuliiahula12@gmail.com>
2026-06-30 19:30:34 +0200

    Zaimplementowano modul "Filmy", "Klienci", "Dziennik wypożyczeń"

--- src/main/java/com/example/retroshopmanager/MainController.java
+++ src/main/java/com/example/retroshopmanager/MainController.java
@@ -8,9 +8,18 @@ import javafx.scene.layout.BorderPane;
 import java.io.IOException;
 
 public class MainController {
-
     @FXML
     private BorderPane mainBorderPane;
+
+    @FXML
+    public void initialize() {
+        loadPage("StartView.fxml");
+    }
+
+    @FXML
+    private void showStartView() {
+        loadPage("StartView.fxml");
+    }
 
     @FXML
     private void showMusicView() {
@@ -18,8 +27,23 @@ public class MainController {
     }
 
     @FXML
+    private void showMoviesView() {
+        loadPage("MovieView.fxml");
+    }
+
+    @FXML
     private void showBooksView() {
         loadPage("BookView.fxml");
+    }
+
+    @FXML
+    private void showCustomersView() {
+        loadPage("CustomerView.fxml");
+    }
+
+    @FXML
+    private void showRentalsView() {
+        loadPage("RentalView.fxml");
     }
 
     private void loadPage(String page) {
@@ -28,7 +52,7 @@ public class MainController {
             mainBorderPane.setCenter(root);
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Nie udało się załadować pliku: " + page);
+            System.out.println("Nie udalo sie zaladowac pliku: " + page);
         }
     }
 }
